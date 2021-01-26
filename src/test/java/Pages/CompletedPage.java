3ef192ff6807a7bad29e18df2a102e6a85bd0359
Oliver Jean <43223926+cooliver81@users.noreply.github.com>
2021-01-26 15:53:51 -0500

    added comments to completed page

--- src/test/java/Pages/CompletedPage.java
+++ src/test/java/Pages/CompletedPage.java
@@ -9,12 +9,14 @@ public class CompletedPage extends Page {
 		super(driver);
 		// TODO Auto-generated constructor stub
 	}
-
+	
+	// Function to navigate to the All Page from Completed Page
 	public void navigateToAllPage() {
 		allButton = driver.findElement(By.xpath("//a[@href='#/']"));
 		allButton.click();
 	}
 	
+	// Function to navigate to the Active Page from Completed Page
 	public void navigateToActivePage() {
 		activeButton = driver.findElement(By.xpath("//a[@href='#/active']"));
 		activeButton.click();
